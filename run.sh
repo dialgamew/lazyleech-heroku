2d6e021fd42456f4e68492af008d70f433ba6905
dialgamew <77679123+dialgamew@users.noreply.github.com>
2021-09-18 18:21:00 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 touch aria2.log lazyleech.log
 tail -f aria2.log &
 tail -f lazyleech.log &
-aria2c --enable-rpc=true -j5 -x5 > aria2.log 2>&1 &
+aria2c --enable-rpc=true -j2 -x5 > aria2.log 2>&1 &
 python3 -m lazyleech > lazyleech.log 2>&1
